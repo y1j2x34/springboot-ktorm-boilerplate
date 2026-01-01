97de0c2ac2f32e0df0545fc3f3dc9eb67212ee46
y1j2x34 <y1j2x34@qq.com>
2026-01-01 16:23:32 +0800

    Add @JsonIgnore to password field in UserInfoDto to prevent sensitive data exposure

--- user/user-api/src/main/kotlin/com/vgerbot/user/dto/UserInfoDto.kt
+++ user/user-api/src/main/kotlin/com/vgerbot/user/dto/UserInfoDto.kt
@@ -1,12 +1,13 @@
 package com.vgerbot.user.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import java.time.Instant
 
 data class UserInfoDto (
     val id: Int,
 
     val username: String,
-
+    @JsonIgnore
     val password: String,
 
     val email: String,
